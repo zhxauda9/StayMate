1d005c5d534bc13adcc45a77424beab05443b0fd
Aida <zhalgasovaida@gmail.com>
2025-01-22 01:25:48 +0500

    Added and created Log in web page!

---
+++ web/scripts/login.js
@@ -0,0 +1,26 @@
+document.getElementById('login-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const user = {
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+    };
+
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user),
+        });
+
+        if (!response.ok) {
+            const errorResponse = await response.json();
+            throw new Error(errorResponse.message || 'Failed to log in.');
+        }
+
+        alert('Logged in successfully!');
+    } catch (error) {
+        console.error(error);
+        alert(`Error: ${error.message}`);
+    }
+});
