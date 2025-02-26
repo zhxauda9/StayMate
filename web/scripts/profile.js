245f79f69742d381958d698e8cee45c21585ce06
Temutjin2k <tamutdzhin2006@mail.ru>
2025-02-26 11:03:59 +0500

    test

--- web/scripts/profile.js
+++ web/scripts/profile.js
@@ -105,7 +105,7 @@ function connectWebSocket() {
         return;
     }
 
-    socket = new WebSocket(`wss://3.75.158.163/ws/user?userID=${encodeURIComponent(ChatUUID)}`);
+    socket = new WebSocket(`wss://staymate-o3g2.onrender.com/ws/user?userID=${encodeURIComponent(ChatUUID)}`);
     socket.onopen = () => console.log("Connected to WebSocket");
 
     socket.onmessage = event => {
